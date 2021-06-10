a37a9cfc777c184c46246af4e36c3a8ca609b5c9
Ian Purton <ian.purton@gmail.com>
2021-06-10 13:49:53 +0000

    otp migrations

---
+++ migrations/2021-06-10-132403_add_otp/down.sql
@@ -0,0 +1,2 @@
+ALTER TABLE sessions DROP COLUMN otp_code;
+ALTER TABLE sessions DROP COLUMN otp_code_confirmed;
---
+++ migrations/2021-06-10-132403_add_otp/up.sql
@@ -0,0 +1,2 @@
+ALTER TABLE sessions ADD otp_code INTEGER NOT NULL DEFAULT (random() * 100000 + 1)::int;
+ALTER TABLE sessions ADD otp_code_confirmed BOOLEAN NOT NULL DEFAULT false;
