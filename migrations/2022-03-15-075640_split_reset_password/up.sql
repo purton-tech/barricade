80acd5473ab7efb9886cca78ebfd30b519d188d5
Ian Purton <ian.purton@gmail.com>
2022-03-15 08:02:20 +0000

    feat(security): We now counter timing attacks for reset password with a split token.

---
+++ migrations/2022-03-15-075640_split_reset_password/up.sql
@@ -0,0 +1,3 @@
+UPDATE TABLE bcrypt_users DROP COLUMN reset_password_token;
+UPDATE TABLE bcrypt_users ADD COLUMN reset_password_selector VARCHAR;
+UPDATE TABLE bcrypt_users ADD COLUMN reset_password_validsator_hash VARCHAR;
