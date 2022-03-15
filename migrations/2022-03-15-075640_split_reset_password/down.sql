8a90066e3371c12ce7cf45a56d39c0b45c0551f1
Ian Purton <ian.purton@gmail.com>
2022-03-15 08:55:45 +0000

    Start to use a split verifier.

--- migrations/2022-03-15-075640_split_reset_password/down.sql
+++ migrations/2022-03-15-075640_split_reset_password/down.sql
@@ -1,3 +1,3 @@
-UPDATE TABLE bcrypt_users ADD COLUMN reset_password_token;
-UPDATE TABLE bcrypt_users DROP COLUMN reset_password_selector;
-UPDATE TABLE bcrypt_users DROP COLUMN reset_password_validsator_hash;
+ALTER TABLE bcrypt_users ADD COLUMN reset_password_token UUID;
+ALTER TABLE bcrypt_users DROP COLUMN reset_password_selector;
+ALTER TABLE bcrypt_users DROP COLUMN reset_password_validator_hash;
