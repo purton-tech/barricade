0be8b050cde0b67033eed95ddfb25e6147497581
Ian Purton <ian.purton@gmail.com>
2023-02-10 10:42:51 +0000

    Axum connected to cornucopia

---
+++ crates/axum-server/src/main.rs
@@ -0,0 +1,38 @@
+mod config;
+mod errors;
+
+use crate::errors::CustomError;
+use axum::{extract::Extension, response::Json, routing::get, Router};
+use std::net::SocketAddr;
+use db::User;
+
+#[tokio::main]
+async fn main() {
+    let config = config::Config::new();
+
+    let pool = db::create_pool(&config.database_url);
+
+    // build our application with a route
+    let app = Router::new()
+        .route("/", get(users))
+        .layer(Extension(config))
+        .layer(Extension(pool.clone()));
+
+    // run it
+    let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
+    println!("listening on {}", addr);
+    axum::Server::bind(&addr)
+        .serve(app.into_make_service())
+        .await.unwrap();
+}
+
+async fn users(Extension(pool): Extension<db::Pool>) -> Result<Json<Vec<User>>, CustomError> {
+    let client = pool.get().await?;
+
+    let users = db::queries::users::get_users()
+        .bind(&client)
+        .all()
+        .await?;
+
+    Ok(Json(users))
+}
